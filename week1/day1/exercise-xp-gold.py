970f6ddefada8006ca70561d33be14cc6bb97155
Gabby <gabbyzuck@gmail.com>
2025-03-15 17:31:18 +0200

    Exercise XP Gold Completed

---
+++ week1/day1/exercise-xp-gold.py
@@ -0,0 +1,13 @@
+#Exercise 1
+print(("Hello world " + "I love python ") * 4)
+#Exercise 2
+month = int(input("Enter a month (1 to 12): "))
+if month == 3 or month == 4 or month == 5:
+    print("Spring")
+elif month == 6 or month == 7 or month == 8:
+    print("Summer")
+elif month == 9 or month == 10 or month == 11:
+    print("Fall")
+else:
+    print("Winter")
+
